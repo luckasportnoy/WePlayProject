4968f99efeea2464be7ef40efaf0f2e1fb54475f
luckasportnoy <lucas.cambraia.dt@hotmail.com>
2014-11-08 20:54:20 +0000

    primeiro commit

---
+++ WePlay/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^$', 'WePlay.views.home', name='home'),
+    # url(r'^WePlay/', include('WePlay.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+
+)
