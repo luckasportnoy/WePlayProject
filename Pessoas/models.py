8624914bedee2794ead71e759ee214551871598e
NetoRpg <NTOProducoes@gmail.com>
2014-11-08 20:50:33 -0200

    Algumas Modificações

--- Pessoas/models.py
+++ Pessoas/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-class Pessoa(models.Model)
+class Pessoa(models.Model):
     nome = models.CharField(max_length=200)
     sobrenome = models.CharField(max_length=200)
     nickname = models.CharField(max_length=200)
     password = models.CharField(max_length=18)
     
-class Jogo(models.Model)
+class Jogo(models.Model):
     nome = models.CharField(max_length=200)
     genero = models.CharField(max_length=200)
     data_lancamento = models.DateTimeField()
+    imagem = models.ImageField(upload_to='GameImages')
